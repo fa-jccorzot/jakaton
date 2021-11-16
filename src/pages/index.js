0a9c483df1696f67d4bf433e60ea961046efde03
ext_jccorzot@falabella.cl <ext_jccorzot@falabella.cl>
2021-11-16 00:03:22 -0500

    banner

--- src/pages/index.js
+++ src/pages/index.js
@@ -3,46 +3,92 @@ import Header from '../components/header';
 import Head from '../components/head';
 import Footer from '../components/footer';
 
+import CustomLink from '../components/link';
+import Left from '../components/arrows/left';
+import Right from '../components/arrows/right';
+
+const brands = [
+  {
+    id: 1,
+    img: './images/brands/unsplash.png'
+  },
+  {
+    id: 2,
+    img: './images/brands/nike.png'
+  },
+  {
+    id: 3,
+    img: './images/brands/farfetch.png'
+  },
+  {
+    id: 4,
+    img: './images/brands/uniqlo.png'
+  },
+  {
+    id: 5,
+    img: './images/brands/west-elm.png'
+  },
+  {
+    id: 6,
+    img: './images/brands/gap.png'
+  }
+];
+
 export default function Home() {
   return (
     <>
       <Head title={'Surprise Me'} />
       <Header />
       <main className={styles.main}>
-        <h1 className={styles.title}>
-          Welcome to <a href="https://nextjs.org">Next.js!</a>
-        </h1>
+        <section className={styles.bannerContainer}>
+          <img className={styles.bannerImg} src={'./images/banner.png'} alt={'banner'} />
+        </section>
 
-        <p className={styles.description}>
-          Get started by editing <code className={styles.code}>pages/index.js</code>
-        </p>
+        <Section title={'Lo último que buscaste'}></Section>
 
-        <div className={styles.grid}>
-          <a href="https://nextjs.org/docs" className={styles.card}>
-            <h2>Documentation &rarr;</h2>
-            <p>Find in-depth information about Next.js features and API.</p>
-          </a>
-
-          <a href="https://nextjs.org/learn" className={styles.card}>
-            <h2>Learn &rarr;</h2>
-            <p>Learn about Next.js in an interactive course with quizzes!</p>
-          </a>
-
-          <a href="https://github.com/vercel/next.js/tree/master/examples" className={styles.card}>
-            <h2>Examples &rarr;</h2>
-            <p>Discover and deploy boilerplate example Next.js projects.</p>
-          </a>
-
-          <a
-            href="https://vercel.com/new?utm_source=create-next-app&utm_medium=default-template&utm_campaign=create-next-app"
-            className={styles.card}
-          >
-            <h2>Deploy &rarr;</h2>
-            <p>Instantly deploy your Next.js site to a public URL with Vercel.</p>
-          </a>
-        </div>
+        <Section title={'Tus marcas favoritas'}>
+          <div className={styles.brandsContainer}>
+            {brands.map(brand => (
+              <div key={brand.id} className={styles.brandContainer}>
+                <img src={brand.img} alt={'brand'} />
+              </div>
+            ))}
+          </div>
+        </Section>
+        <Section title={'Inspirado en tus favoritos'}></Section>
       </main>
       <Footer />
     </>
+  );
+}
+
+function Section({ title, children }) {
+  return (
+    <div className={styles.section}>
+      <h2 className={styles.sectionTitle}>{title}</h2>
+      {children}
+    </div>
+  );
+}
+
+function ProductCard({ brand, name, price, img }) {
+  return (
+    <div className={styles.productContainer}>
+      <div src={img} className={styles.productImage} />
+      <p className={styles.productBrand}>{brand}</p>
+      <p className={styles.productName}>{name}</p>
+      <p className={styles.productPrice}>{price}</p>
+      <CustomLink className={styles.productButton}>Ver Producto</CustomLink>
+    </div>
+  );
+}
+
+function ProductsContainer({ children }) {
+  return (
+    <div>
+      <Left />
+      {children}
+      <Right />
+    </div>
   );
 }
